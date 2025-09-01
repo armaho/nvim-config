6143d1fd91b3b3a93683f94c97ab6830f149efa0
Arman Hosseini <armanhosseini878787@gmail.com>
2025-09-01 17:07:30 +0330

    change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,8 @@
 return { 
-	"catppuccin/nvim", 
-	name = "catppuccin", 
-	priority = 1000, 
+  "catppuccin/nvim", 
+  name = "catppuccin", 
+  priority = 1000, 
 	config = function()
-		vim.cmd.colorscheme("catppuccin-frappe")
+		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
